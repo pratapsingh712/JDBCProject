e25bc833e53e08a930110304237cefe294299a42
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-09-14 12:49:57 +0530

    Added prepared statement

--- src/preparedStatement/PreparedStatements.java
+++ src/preparedStatement/PreparedStatements.java
@@ -10,21 +10,27 @@ public class PreparedStatements {
         String uname = "postgres";
         String pass = "Raghav@1234";
 
-        int sid = 101;
-        String sname = "Max";
-        int marks = 48;
+        int sid = 102;
+        String sname = "Maxy";
+        int marks = 78;
 
-        String sql = "insert into student values(" +sid +", '" +sname +"', "+marks+ ")"; // can lead to sql injection and this way of inserting is very confusing
+        //String sql = "insert into student values(" +sid +", '" +sname +"', "+marks+ ")"; // can lead to sql injection and this way of inserting is very confusing
         // we need to keep in mind that we should also think about performance optimisation and it can be achieved with the help of caching the query -> prepared statement
+
+        String sql = "insert into student values (?,?,?)";
 
         Class.forName("org.postgresql.Driver");
 
         Connection con = DriverManager.getConnection(url,uname,pass);
         System.out.println("Connection Established!");
 
-        Statement st = con.createStatement();
+        //Statement st = con.createStatement();
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setInt(1,sid);
+        st.setString(2,sname);
+        st.setInt(3,marks);
 
-        st.execute(sql);
+        st.execute();
 
         con.close();
 
